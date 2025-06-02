cacea9a3cd3035a743ec6760b632f4d658ab61e3
mariapcaleffi <mcpcaleffi@gmail.com>
2025-06-02 16:24:24 -0300

    carrinho

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Carrinnho de compras, situação de aprendizagem front-end.
 
 ## Link do Figma
+
+https://www.figma.com/design/XCusB3lAISoot8yiHdIxkM/prototipo-lojinha-carrinho?node-id=0-1&t=VxqKcwEIreZAfESZ-1
---
+++ web/dados.js
@@ -0,0 +1,23 @@
+[
+  {
+    "id": "1",
+    "name": "Long sleeves",
+    "price": "99,99",
+    "description": "Uma blusa de mangas longas confortável.",
+    "image": "https://i.pinimg.com/736x/c8/7e/db/c87edbbb5c30045472f610805cfbae55.jpg"
+  },
+  {
+    "id": "2",
+    "name": "Saia longa",
+    "price": "109,99",
+    "description": "Uma saia longa elegante.",
+    "image": "https://i.pinimg.com/736x/2e/bc/73/2ebc73e9695c12a3a7ea2819b7f4bc6f.jpg"
+  },
+  {
+    "id": "3",
+    "name": "Radiohead Shirt",
+    "price": "39,99",
+    "description": "Camisa da banda Radiohead.",
+    "image": "https://i.pinimg.com/736x/40/85/c7/4085c7b6142d71d81c3062eaa2c3d1c2.jpg"
+  }
+]
